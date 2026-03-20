493b33bc7104d12b5609c1ff4658595e28a36f0d
TaniaErato <utm23090802@utma.edu.mx>
2026-03-20 17:30:13 -0600

    fix db connection mysql_url

--- config/db.js
+++ config/db.js
@@ -1,19 +1,11 @@
 const mysql = require("mysql2");
 
-// Pool de conexiones (mejor práctica)
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
+  uri: process.env.MYSQL_URL,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
-console.log("HOST:", process.env.MYSQLHOST);
-console.log("USER:", process.env.MYSQLUSER);
 
 // Exportamos con promesas (async/await)
 module.exports = pool.promise();
